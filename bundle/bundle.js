c93fa52a1bd4a7b7455d0173782cb998f61cf5b3
Jordan Etzler <jozza24@gmail.com>
2017-02-12 20:19:53 +1100

    Updated Background

--- bundle/bundle.js
+++ bundle/bundle.js
@@ -49,22 +49,43 @@
 	$('.double_modal').hide();
 
 	$(".hero_button").click(function () {
-	  $('html, body').animate({
-	    scrollTop: $(".study_one").offset().top
-	  }, 1000, "swing");
+	    $('html, body').animate({
+	        scrollTop: $(".study_one").offset().top
+	    }, 1000, "swing");
 	});
 
 	$('.exit_modal').click(function () {
-	  $('.double_modal').fadeOut();
+	    $('.double_modal').fadeOut();
 	});
 
 	$('.cta_button').click(function () {
-	  $('.double_modal').fadeIn();
+	    $('.double_modal').fadeIn();
 	});
 
-	// $('#specialty_dropdown').hover(function() {
-	//   $('.hover_menu').show();
-	// })
+	$("#smm_button").on('click', function () {
+	    $('html, body').animate({
+	        scrollTop: $(".study_one").offset().top
+	    }, 1000, "swing");
+	    console.log('hey');
+	});
+
+	$("#growth_button").click(function () {
+	    $('html, body').animate({
+	        scrollTop: $(".study_two").offset().top
+	    }, 1000, "swing");
+	});
+
+	$("#marketing_button").click(function () {
+	    $('html, body').animate({
+	        scrollTop: $(".study_three").offset().top
+	    }, 1000, "swing");
+	});
+
+	// $("#web_button").click(function() {
+	//     $('html, body').animate({
+	//         scrollTop: $(".study_one").offset().top
+	//     }, 1000, "swing");
+	// });
 
 /***/ }
 /******/ ]);
